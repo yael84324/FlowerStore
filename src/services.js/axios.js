ba0fed171426862b00b0d46e1941815ce29caf21
yael katzanellenbogen <yael84324@gmail.com>
2023-06-09 00:02:42 +0300

    original url

--- src/services.js/axios.js
+++ src/services.js/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// const axiosInstans = axios.create({ baseURL: 'https://localhost:8000' })
-const axiosInstans = axios.create({ baseURL: 'https://storeproductsserver.onrender.com' })
+const axiosInstans = axios.create({ baseURL: 'https://localhost:8000' })
 axiosInstans.interceptors.request.use((req) => {
     return req
 })
